fe6da0d0e36776d7692bf502de6dbaa7976eb636
Bart Huntley <barthuntley@iinet.net.au>
2021-11-05 11:29:26 +0800

    select inputs

--- app.R
+++ app.R
@@ -3,19 +3,29 @@ library(shiny)
 load("./dat.RData")
 
 ui <- fluidPage(
+  selectInput(inputId = "sid", "Select Somethings",
+              choices = c("t1" = "Toyota Corolla",
+                          "t2" = "Toyota Corona"),
+              selected = NULL,
+              multiple = TRUE
+              ),
   downloadButton("downloadData", "Download")
 )
 
 server <- function(input, output) {
   # Our dataset
-  data <- mtcarsDF
+  # data <- mtcarsDF
+
+  datasetInput <- reactive({
+    mtcarsDF %>% filter(car %in% input$sid)
+  })
 
   output$downloadData <- downloadHandler(
     filename = function() {
       paste("data-", Sys.Date(), ".csv", sep="")
     },
     content = function(file) {
-      write.csv(data, file, row.names = FALSE)
+      write.csv(datasetInput(), file, row.names = FALSE)
     }
   )
 }
